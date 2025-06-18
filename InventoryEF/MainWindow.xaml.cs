aa84e05b3740546221be93cc4653889ca4a1f77f
lukasprogram <lukas.je@post.cz>
2025-06-19 00:08:44 +0200

    Editing works

--- InventoryEF/MainWindow.xaml.cs
+++ InventoryEF/MainWindow.xaml.cs
@@ -46,6 +46,58 @@ namespace InventoryEF
             vm.DeleteBook((Book)Inventory.SelectedItem);
         }
 
+        bool buttonsVisible = true;
+        private void EditBook(object sender, RoutedEventArgs e) 
+        {
+            buttonsVisible = !buttonsVisible;
+            if (buttonsVisible)
+            {
+                AddBookButton.Visibility = Visibility.Visible;
+                DeleteBookButton.Visibility = Visibility.Visible;
+
+                SaveButton.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                AddBookButton.Visibility = Visibility.Hidden;
+                DeleteBookButton.Visibility = Visibility.Hidden;
+
+                SaveButton.Visibility = Visibility.Visible;
+            }
+
+            if (!buttonsVisible)
+            {
+                vm.StartEditing((Book)Inventory.SelectedItem);
+            } else
+            {
+                vm.StopEditing();
+            }
+            
+
+        }
+        private void SaveBook(object sender, RoutedEventArgs e)
+        {
+            buttonsVisible = !buttonsVisible;
+            if (buttonsVisible)
+            {
+                AddBookButton.Visibility = Visibility.Visible;
+                DeleteBookButton.Visibility = Visibility.Visible;
+
+                SaveButton.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                AddBookButton.Visibility = Visibility.Hidden;
+                DeleteBookButton.Visibility = Visibility.Hidden;
+
+                SaveButton.Visibility = Visibility.Visible;
+            }
+
+            vm.SaveEditChanges();
+            
+
+        }
+
     }
 
 
@@ -61,7 +113,18 @@ namespace InventoryEF
         public string Author { get { return author; } set { author = value; OnPropertyChanged(nameof(Author)); } }
 
         string _price;
-        public string Price { get { return _price; } set { _price = value; OnPropertyChanged(nameof(_price)); price = int.Parse(_price); } }
+        public string Price { get { return _price; } set 
+            { 
+                _price = value; 
+                OnPropertyChanged(nameof(_price)); 
+                if(int.TryParse(_price, out int priceParsed))
+                {
+                    price = priceParsed; 
+                }
+                
+                
+            } 
+        }
 
         int price;
 
@@ -81,7 +144,9 @@ namespace InventoryEF
             using (var context = new LibraryContext())
             {
                var book = new Book() { Author = author, Price = price, Title = title };
-
+                if (string.IsNullOrEmpty(book.Author) || book.Price == 0 || string.IsNullOrEmpty(book.Title) ) {
+                    return;
+                }
                context.Books.Add(book);
                BooksInventory.Add(book);
                context.SaveChanges();
@@ -106,6 +171,59 @@ namespace InventoryEF
             OnPropertyChanged(nameof(BooksInventory));
         }
 
+        (string, string, string) SavedInput;
+        Book editedBook;
+        public void StartEditing(Book book)
+        {
+
+            SavedInput = (Title, Author, Price);
+
+            if (BooksInventory.Contains(book))
+            {
+                Title = book.Title;
+                Author = book.Author;
+                Price = book.Price + "";
+            }
+
+            editedBook = book;
+        }
+
+        public void StopEditing()
+        {
+            Title = SavedInput.Item1;
+            Author = SavedInput.Item2;
+            Price = SavedInput.Item3;
+
+            editedBook = null;
+        }
+
+        public void SaveEditChanges()
+        {
+            if (editedBook == null) { return; }
+
+            if (BooksInventory.Contains(editedBook))
+            {
+                using (var context = new LibraryContext()) 
+                {
+                    Book foundBook = context.Books.ToList().Find(x => x.Id == editedBook.Id);
+
+                    if (foundBook != null) {
+
+                        foundBook.Title = Title;
+                        foundBook.Author = Author;
+                        foundBook.Price = double.Parse(Price);
+
+                        
+                        context.SaveChanges();
+                    }
+                }
+
+                LoadInventoryFromDbAsync();
+            }
+
+            StopEditing();
+        }
+
         public event PropertyChangedEventHandler PropertyChanged;
 
         protected void OnPropertyChanged(string name)
@@ -124,4 +242,4 @@ namespace InventoryEF
 
     
 
-    }
+}
